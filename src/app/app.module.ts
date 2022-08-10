88f61a1f5e704fe07b724a8dcf7c3c38ff857927
netMikebrowski <m.lojewski@netbots.no>
2022-08-10 11:44:27 +0200

    All of the projects combined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { Panel2Component } from './panel2/panel2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    ButtonsComponent,
+    Panel2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/panel2/panel2.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'boostrap-app-panel2',
+  templateUrl: './panel2.component.html',
+  styleUrls: ['./panel2.component.css']
+})
+export class Panel2Component implements OnInit {
+
+  constructor() { }
+
+  whatever="SOMETHING"
+
+  ngOnInit(): void {
+  }
+
+}
